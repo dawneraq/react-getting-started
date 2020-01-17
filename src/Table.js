7ef540af062972e629848403a7c85d8cf59e0b57
dawneraq <dawneraq@gmail.com>
2020-01-16 22:03:13 -0800

    add remove row button to App

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const TableHeader = () => {
   return (
@@ -6,17 +6,21 @@ const TableHeader = () => {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
 }
 
-const TableBody = props => {
+const TableBody = (props) => {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -26,17 +30,18 @@ const TableBody = props => {
   );
 }
 
-class Table extends Component {
-  render() {
-    const { characterData } = this.props;
+const Table = (props) => {
+  const { characterData, removeCharacter } = props;
 
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={characterData} />
-      </table>
-    );
-  }
-}
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={characterData}
+        removeCharacter={removeCharacter}
+      />
+    </table>
+  );
+};
 
 export default Table;
